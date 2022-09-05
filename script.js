333da62a922260ffc98348efc7280c98c7da77c8
Oscar <oscarscouts9@gmail.com>
2022-09-04 21:47:38 -0400

    fixing details

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let color = "black";
+let click = true;
+
 function populateBoard(size){
 
 let board = document.querySelector(".board");
@@ -9,10 +12,8 @@ board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 let amount = size * size;
 for(let i = 0; i < amount; i++){
     let square = document.createElement("div");
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
 }
 }
@@ -21,8 +22,41 @@ populateBoard(16);
 
 function changeSize(input){
     if(input >= 2 || input <= 100){
+        document.querySelector('.error').style.display = 'none';
         populateBoard(input);
     }else{
-        console.log("too many mistakes");
+        document.querySelector('.error').style.display = 'flex';
     }
 }
+
+function colorSquare(){
+    if(click){
+    if(color === "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }else{
+    this.style.backgroundColor = color;
+}
+}
+}
+ 
+function changeColor(choice){
+    color = choice
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+let squares = board.querySelectorAll("div");
+squares.forEach((div) => div.style.backgroundColor= 'white');
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.tagName != 'BUTTON'){
+    click = !click;
+
+    if (click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    }else{
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+}
+})
